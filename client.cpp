ac13fa879bde3815a510de527433fdcaf8ae218c
bentoro <bl_04@hotmail.com>
2017-11-10 15:25:01 -0800

    added second socket

--- client.cpp
+++ client.cpp
@@ -12,32 +12,26 @@
 
 using namespace std;
 
-#define BUFLEN			80  	// Buffer length
+#define BUFLEN			1024  	// Buffer length
+
+
 
 int main (int argc, char *argv[]) {
-    struct hostent	*hp;
-    struct sockaddr_in server;
-    char *bf, buf[BUFLEN];
-    int port, cmdsocket;
-    char *host;
-    string msg;
-    int n, read;
-
-      if(argc != 2){
-        printf("Error : host [port]");
-        exit(1);
-      } else {
-        host = argv[0];
-        port = atoi(argv[1]);
-      }
-
+  struct hostent	*hp;
+  struct sockaddr_in server, server1, client;
+  char *bf, buf[BUFLEN];
+  int port, cmdsocket, datasocket;
+  socklen_t client_len;
+  //char *host;
+  string msg;
+  int n, rd, da;
 
     //create command socket, returns socket file descriptor(int)
     if ((cmdsocket = socket(AF_INET, SOCK_STREAM,0)) == -1){
-      printf("Error creating socket");
+      printf("Error creating socket\n");
       exit(1);
     } else {
-
+      printf("Socket created\n");
     }
     //empties the pointer to the buffer and the size of the buffer is cleared
     bzero((char *)&server, sizeof(struct sockaddr_in));
@@ -48,21 +42,62 @@ int main (int argc, char *argv[]) {
     bcopy(hp->h_addr, (char *)&server.sin_addr, hp->h_length);
     //connect takes socket file descriptor, address of host including port, and the size of the address
     if ((connect(cmdsocket,(struct sockaddr *)&server, sizeof(server))) == -1){
-      printf("Error connectiong to server");
+      printf("Error connectiong to server\n");
       exit(1);
     } else {
-      printf("Connected to server");
+      printf("Connected to server\n");
     }
 
-    printf("Please enter a message to send to the server: ");
-    //cin >> msg;
+    //*******************************************************
+    //datasocket
+    if ((datasocket = socket(AF_INET, SOCK_STREAM,0)) == -1){
+      printf("Error creating socket");
+      exit(1);
+    }else {
+      printf("Socket created \n");
+    }
+    bzero((char *)&server1, sizeof(struct sockaddr_in));
+    server1.sin_family = AF_INET;
+    server1.sin_port = htons(7006);
+    server1.sin_addr.s_addr = htonl(INADDR_ANY);
+    if ((bind(datasocket,(struct sockaddr *)&server1,sizeof(server1))) == -1){
+      printf("Error binding socket");
+      exit(1);
+    } else {
+      printf("Binding socket\n");
+    }
+
+    listen(datasocket,5);
+    client_len = sizeof(client);
+    if ((da = accept(cmdsocket,(struct sockaddr *) &client, &client_len)) == -1){
+      printf("Error accepting client");
+      exit(1);
+    } else {
+      printf("Client connected\n");
+    }
+    //datasocket
+    //*******************************************************
+
+    printf("Please filename to transfer: ");
     fgets(buf, BUFLEN, stdin);
     send (cmdsocket, buf, BUFLEN, 0);
     bzero(buf, BUFLEN);
-    bf = buf;
-    read = BUFLEN;
+    /*bf = buf;
+    rd = BUFLEN;
     while ((n = recv(cmdsocket, bf, sizeof(buf), 0)) < 0){
 
     }
-    printf ("sending:%s\n", buf);
-}
+    printf ("sending:%s\n", buf);*
+    string filename;
+    cin >> filename;
+    FILE *file;
+    if((file=fopen(filename,"rb")) == NULL){
+      printf("File doesn't exist");
+    }
+
+    //fill buffer with null characters
+    memset(buf,'\0', BUFLEN);
+    */
+
+
+    }
--- server.cpp
+++ server.cpp
@@ -6,28 +6,32 @@
 #include <strings.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <errno.h>
 
 #define BUFLEN			80  	// Buffer length
 #define SVR_PRT 7005
 int main (int argc, char *argv[]) {
-  struct sockaddr_in server, client;
-  int port, cmdsocket, sd;
+  struct hostent	*hp;
+  struct sockaddr_in server, client, server1;
+  int port, cmdsocket, sd, datasocket;
   socklen_t client_len;
-  char *host;
+  //char *host;
   char *bf, buf[BUFLEN];
   int n, read;
 
-  if(argc != 0){
+  /*if(argc != 0){
     printf("Error : [port]");
     exit(1);
   } else {
     port = atoi(argv[0]);
-  }
+  }*/
   if ((cmdsocket = socket(AF_INET, SOCK_STREAM,0)) == -1){
     printf("Error creating socket");
     exit(1);
+  }else {
+    printf("Socket created \n");
   }
-  printf("CONNECTIONG TO CLIENT");
   bzero((char *)&server, sizeof(struct sockaddr_in));
   server.sin_family = AF_INET;
   server.sin_port = htons(7005);
@@ -36,7 +40,7 @@ int main (int argc, char *argv[]) {
     printf("Error binding socket");
     exit(1);
   } else {
-    printf("Binding socket");
+    printf("Binding socket\n");
   }
 
   listen(cmdsocket,5);
@@ -45,18 +49,46 @@ int main (int argc, char *argv[]) {
     printf("Error accepting client");
     exit(1);
   } else {
-    printf("Client connected");
+    printf("Client connected\n");
   }
+
+
+  //********************************************************
+  //data socket
+  //create command socket, returns socket file descriptor(int)
+  if ((datasocket = socket(AF_INET, SOCK_STREAM,0)) == -1){
+    printf("Error creating socket\n");
+    exit(1);
+  } else {
+    printf("Socket created\n");
+  }
+  //empties the pointer to the buffer and the size of the buffer is cleared
+  bzero((char *)&server1, sizeof(struct sockaddr_in));
+  server1.sin_family = AF_INET;
+  server1.sin_port = htons(7006);
+  hp = gethostbyname("127.0.0.1");
+  //copies the host address into the hostent struct
+  bcopy(hp->h_addr, (char *)&server1.sin_addr, hp->h_length);
+  //connect takes socket file descriptor, address of host including port, and the size of the address
+  if ((connect(datasocket,(struct sockaddr *)&server1, sizeof(server1))) == -1){
+    printf("Error connectiong to server\n");
+    exit(1);
+  } else {
+    printf("Connected to server\n");
+  }
+  //data socket
+  //********************************************************
+
 
   bf = buf;
   read = BUFLEN;
   while ((n = recv(sd, bf, sizeof(buf), 0)) < 0){
-
   }
-  printf ("sending:%s\n", buf);
-  printf("Please enter a message to send to the server: ");
+  printf ("client:%s\n", buf);
+
+  /*printf("Please enter a message to send to the server: ");
   bzero(buf,sizeof(buf));
   fgets(buf, BUFLEN, stdin);
-  send(sd, buf, BUFLEN, 0);
+  send(sd, buf, BUFLEN, 0);*/
 
 }
